e15c5209b3a2d5e4db54cea04c291bff7e921e6b
Yacub Ali <Yacub93@Yacubs-MacBook-Pro.local>
2017-05-17 20:28:56 +0100

    Display weather data with unit conversions

--- js/weather.js
+++ js/weather.js
@@ -1,36 +1,48 @@
 var classApp = angular.module('weatherApp', []);
-// angular.module('weatherCtrl', ['ngRoute']);
+
 classApp.controller("weatherCtrl", function($scope, $http) {
-  var vm = $scope;
-  var vm = $scope;
-  // vm.test = "Hello, Yacub Ali";
-  vm.weatherInfo = {
+  var obj = $scope;
+
+  // obj.test = "Hello, Yacub Ali";
+  obj.weatherInfo = {
     heading: "Open Weather API Project",
     subHeading1: "Show Local Weather to user",
     subHeading2: {
-      name: "check out my GitHub page",
+      name: "Check out my GitHub profile:",
       link: "https://github.com/Yacub93"
     }
   };
-    // $http.jsonp("http://ipinfo.io/json?callback=JSON_CALLBACK");
-  //   $http.get("http://ip-api.com/json").success(function(data) {
-  //   vm.lat = data.lat;
-  //   vm.lon = data.lon;
-  //     console.log(vm.lat);
-  //   var apiKey = "f75c54724def452edcf8b78c344fe7ed";
-  //   var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + vm.lat + "&lon=" + vm.lon + "&appid=" + apiKey;
+
+    $http.get("http://ip-api.com/json").success(function(data) {
+    obj.lat = data.lat;
+    obj.lon = data.lon;
+
+    var apiKey = "f75c54724def452edcf8b78c344fe7ed";
+    var openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?lat=" + obj.lat + "&lon=" + obj.lon + "&appid=" + apiKey;
    
-  //   $http.get(openWeatherURL).success(function(data) {
-  //         // console.log(openWeatherUrl);
+    $http.get(openWeatherURL).success(function(data) {
+          // console.log(openWeatherURL);
 
+    obj.description = data.weather[0].description;
+    obj.speed = data.wind.speed;
 
+    // Convert knot to mph/kph
+    obj.mphSpeed = (2.237 * data.wind.speed).toFixed(1) + "mph";//convert to mph 1dp place
+    obj.kphSpeed = (1.852 * data.wind.speed).toFixed(1) + "kph";//convert to kph 1dp place
+    // console.log(obj.speed);
+    obj.location = data.name;
+    obj.temp = data.main.temp;
+    // console.log(obj.temp);
 
-  //  });
+    // Convert kelvin to Fahrenheit/Celsius
+    obj.cTemp = (obj.temp - 273).toFixed(1) + "(°C)";
+    obj.fTemp = (obj.temp * (9/5) - 459.67).toFixed(1) + "(°F)";
+   });
 
-  // });
+  });
 });         
     
-// //Latest angular version:
+// TODO use latest angular version:
 //   $http({
 //   		method: 'GET',
 //  		url: 'http://ip-api.com/json'
